4f8ee623dec1383def795e29de0a2f531c5e4768
Josh <61065503+joshualevitas@users.noreply.github.com>
2023-05-21 14:44:13 -0500

    renaming feedback

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom"
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components"
+import { About, Contact, Experience, Feedback, Hero, Navbar, Tech, Works, StarsCanvas } from "./components"
 
 const App = () => {
 
@@ -15,7 +15,7 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
+        <Feedback />
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
