ce0b6b3b545f1ccabdbb68641ef33724d3e03e25
Josh <joshualevitas@gmail.com>
2023-05-22 18:12:01 -0500

    started works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
+import { github } from "../assets"
+import { SectionWrapper }  from "../hoc"
+import { projects } from "../constants"
+import { fadeIn, textVariant } from "../utils/motion"
+
+
 const Works = () => {
   return (
     <div>Works</div>
